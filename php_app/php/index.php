ec0c905c6e069d576941206586cd7865729a436b
Kremena Stoilova <kremena.stoilova@gmail.com>
2021-10-16 13:21:55 +0300

    PHP app files, containing the docker_compose.yml file + Dockerfile + index.php (script file)

--- php_app/php/index.php
+++ php_app/php/index.php
@@ -10,7 +10,11 @@ try
     $connect = new PDO("mysql:host=$host;dbname=$db",$user,$password);
     $connect ->setAttribute(PDO::ATTR_ERRMODE,  PDO::ERRMODE_EXCEPTION);
     
-    echo 'Success!';
+    echo 'Hello ';
+    $var = $_GET['id'];
+    $stmt = $connect->query("SELECT name FROM names where id='$var'");
+    $row = $stmt->fetch();
+    echo $row['name'];
     
 }
 catch(PDOException $error)
